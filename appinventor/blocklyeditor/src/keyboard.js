a296ef1760c963753f9287a1cd03f2490b5f8dc7
Emily Giurleo <egiurleo@mit.edu>
2017-03-09 16:07:14 -0500

    going into the workspace and using the right arrow key to select blocks works

--- appinventor/blocklyeditor/src/keyboard.js
+++ appinventor/blocklyeditor/src/keyboard.js
@@ -14,43 +14,78 @@ goog.require('goog.events.KeyCodes');
 goog.require('goog.events.KeyHandler');
 goog.require('goog.window');
 
-Blockly.Keyboard.selectFirstBlock = function(keyCode) {
-  // arrow key logic
-  // if down arrow
-  //    if nothing selected: select first block in top blocks
-  //    if something selected
-  // if up arrow
-  //    if nothing selected: nothing
-  //    if something selected
-  // if right arrow
-  //    if something selected:
-  // if left arrow
-  //    if something selected:
+Blockly.Keyboard.currentBlocksLevel = [];
+Blockly.Keyboard.currentBlocksIndex = -1;
+
+Blockly.Keyboard.workspaceKeyboardInteraction = function(keyCode) {
 
   var keyCodes = {
     DOWN: 40,
     UP: 38,
     RIGHT: 39,
     LEFT: 37
-  }
+  };
 
   if(keyCode == keyCodes.DOWN) {
     if(Blockly.selected) { // if there is a selected block on the workspace
-
+      //DO SOMETHING
     } else {
-      var topBlocks = Blockly.mainWorkspace.getTopBlocks();
-      topBlocks[0].select();
+      this.selectFirstBlockInWorkspace();
     }
+  } else if(keyCode == keyCodes.UP) {
 
-
-  } elseif(keyCode == keyCodes.UP) {
-
-  } elseif(keyCode == keyCodes.RIGHT) {
-
-  } elseif(keyCode == keyCodes.LEFT) {
+  } else if(keyCode == keyCodes.RIGHT) {
+    Blockly.Keyboard.selectNextBlockInLevel();
+  } else if(keyCode == keyCodes.LEFT) {
 
   }
+}
 
+Blockly.Keyboard.selectFirstBlockInWorkspace = function() {
+  Blockly.Keyboard.currentBlocksLevel = Blockly.mainWorkspace.getTopBlocks();
+  Blockly.Keyboard.currentBlocksIndex = 0;
+  Blockly.Keyboard.currentBlocksLevel[Blockly.Keyboard.currentBlocksIndex].select();
+}
 
+Blockly.Keyboard.selectNextBlockInLevel = function() {
+  Blockly.Keyboard.unselectSelectedBlock();
+  Blockly.Keyboard.currentBlocksIndex = Blockly.Keyboard.wrapIncrement(Blockly.Keyboard.currentBlocksLevel, Blockly.Keyboard.currentBlocksIndex);
+  Blockly.Keyboard.currentBlocksLevel[Blockly.Keyboard.currentBlocksIndex].select();
+}
 
+Blockly.Keyboard.selectPreviousBlockInLevel = function() {
+  Blockly.Keyboard.unselectSelectedBlock();
+}
+
+Blockly.Keyboard.selectFirstBlockInNextLevel = function() {
+  //TODO: go down a level in hierarchy
+}
+
+Blockly.Keyboard.selectFirstBlockInPreviousLevel = function() {
+  //TODO: go up a level in hierarchy
+}
+
+/* * * * * * * * * *
+ *  UTIL FUNCTIONS *
+ * * * * * * * * * *
+ */
+
+Blockly.Keyboard.unselectSelectedBlock = function() {
+  Blockly.selected.unselect();
+  Blockly.selected = null;
+}
+
+/*
+ * wrapIncrement
+ * @param list [Array] - a list
+ * @param idx [int] - an index in the list given
+ *
+ * return the incremented index, or, if the index is going off the list, 0
+ */
+Blockly.Keyboard.wrapIncrement = function(list, idx) {
+  if(idx + 1 == list.length) {
+    return 0;
+  } else {
+    return idx + 1;
+  }
 }
